05f3236afdd6ade08f73c75c70b44d975143991a
NubieLazyBill <pit2007@inbox.ru>
2026-08-14 18:08:11 +0500

    1й запуск схемы

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     application
-    id("org.jetbrains.kotlin.jvm") version "1.9.23"
-    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    //id("org.openjfx.javafxplugin") version "0.2.0"
 }
 
 group = "org.example"
@@ -14,7 +14,6 @@ repositories {
 
 val junitVersion = "5.10.2"
 
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
@@ -22,18 +21,26 @@ tasks.withType<JavaCompile> {
 application {
     mainClass.set("org.example.defectmap.HelloApplication")
 }
+
 kotlin {
     jvmToolchain(17)
 }
 
-javafx {
-    version = "21"
-    modules = listOf("javafx.controls", "javafx.fxml")
-}
-
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+    implementation("org.openjfx:javafx-base:21:win")
+    implementation("org.openjfx:javafx-graphics:21:win")
+    implementation("org.openjfx:javafx-controls:21:win")
+    implementation("org.openjfx:javafx-fxml:21:win")
+    implementation("org.openjfx:javafx-web:21:win")
+    implementation("org.openjfx:javafx-swing:21:win")
+    implementation("org.openjfx:javafx-media:21:win")
+
+    implementation("de.codecentric.centerdevice:javafxsvg:1.2.0")
+
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.withType<Test> {
--- src/main/kotlin/org/example/defectmap/HelloController.kt
+++ src/main/kotlin/org/example/defectmap/HelloController.kt
@@ -1,14 +1,33 @@
 package org.example.defectmap
 
 import javafx.fxml.FXML
-import javafx.scene.control.Label
+import javafx.scene.web.WebView
 
 class HelloController {
-    @FXML
-    private lateinit var welcomeText: Label
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private lateinit var webView: WebView
+
+    @FXML
+    private fun initialize() {
+        loadSvgIntoWebView()
+    }
+
+    private fun loadSvgIntoWebView() {
+        val svgFile = javaClass.getResource("/org/example/defectmap/schema.svg")
+        if (svgFile != null) {
+            val html = """
+                <!DOCTYPE html>
+                <html>
+                  <body>
+                    <img src="${svgFile.toExternalForm()}" alt="Schema" style="width:100%; height:auto;" />
+                  </body>
+                </html>
+            """.trimIndent()
+
+            webView.engine.loadContent(html)
+        } else {
+            println("SVG file not found!")
+        }
     }
 }
